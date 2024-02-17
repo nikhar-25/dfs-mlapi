b83059300ae6b69d0eae60c9c07ba5d43aeab1b0
nikhar-25 <nikharms2500@gmail.com>
2024-02-18 04:32:24 +0530

    code commit

--- mlapi/mlapi/mlapi/settings.py
+++ mlapi/mlapi/mlapi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-dmyz!sojhp0sfu_isw=@bnmwo3_&mr0-fvr2n-!pio3(h0!)ch"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
